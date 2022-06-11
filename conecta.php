8de0992629d38deb8d5b43f51dcd9679931e0ebd
Marcos Diniz <marcos.diniz@sicoob.com.br>
2022-06-11 11:16:52 -0300

    teste de conexão com base remota

--- conecta.php
+++ conecta.php
@@ -4,10 +4,25 @@
 // usuario
 // senha
 
+$uri = $_SERVER['REQUEST_URI'];
+$host = $_SERVER['HTTP_HOST'];
+
 $endereco = 'localhost';
 $banco = 'ShalkeXII';
 $usuario = 'postgres';
 $senha = '123456';
+
+echo $host;
+echo $uri;
+
+if ($host == 'https://shalke12.herokuapp.com/')	
+{
+$endereco = 'ec2-34-225-159-178.compute-1.amazonaws.com	';
+$banco = 'd30lvosqrpe06r';
+$usuario = 'nkjcxrpjrvebuw';
+$senha = 'f17c0002ec0c1c4e204e3b786fd4674badd8f3754fef9f5c52af97b50be0f308';
+}
+
 
 try {
   // sgbd:host;port;dbname
